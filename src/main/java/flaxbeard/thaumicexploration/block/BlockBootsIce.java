ca33b5a8f7072df95a7341875672d2695a0afc42
Restyled.io <commits@restyled.io>
2021-11-17 11:15:25 +0000

    Restyled by clang-format

--- src/main/java/flaxbeard/thaumicexploration/block/BlockBootsIce.java
+++ src/main/java/flaxbeard/thaumicexploration/block/BlockBootsIce.java
@@ -1,7 +1,6 @@
 package flaxbeard.thaumicexploration.block;
 
 import java.util.Random;
-
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockIce;
 import net.minecraft.block.material.Material;
@@ -16,54 +15,47 @@ import net.minecraft.world.World;
 
 public class BlockBootsIce extends BlockIce {
 
-	public BlockBootsIce() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+  public BlockBootsIce() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
 
-	
-	@Override
-    public Item getItemDropped(int par1, Random par2Random, int par3)
-    {
-        return Item.getItemFromBlock(Blocks.ice);
+  @Override
+  public Item getItemDropped(int par1, Random par2Random, int par3) {
+    return Item.getItemFromBlock(Blocks.ice);
+  }
+
+  @Override
+  public void harvestBlock(World par1World, EntityPlayer par2EntityPlayer,
+                           int par3, int par4, int par5, int par6) {
+
+    par2EntityPlayer.addExhaustion(0.025F);
+    if (par1World.provider.isHellWorld) {
+      par1World.setBlockToAir(par3, par4, par5);
+      return;
     }
-	
-	@Override
-	public void harvestBlock(World par1World, EntityPlayer par2EntityPlayer, int par3, int par4, int par5, int par6)
-    {
-       
-        par2EntityPlayer.addExhaustion(0.025F);
-            if (par1World.provider.isHellWorld)
-            {
-                par1World.setBlockToAir(par3, par4, par5);
-                return;
-            }
 
-            int i1 = EnchantmentHelper.getFortuneModifier(par2EntityPlayer);
-            this.dropBlockAsItem(par1World, par3, par4, par5, par6, i1);
-            Material material = par1World.getBlock(par3, par4 - 1, par5).getMaterial();
+    int i1 = EnchantmentHelper.getFortuneModifier(par2EntityPlayer);
+    this.dropBlockAsItem(par1World, par3, par4, par5, par6, i1);
+    Material material = par1World.getBlock(par3, par4 - 1, par5).getMaterial();
 
-            if (material.blocksMovement() || material.isLiquid())
-            {
-                par1World.setBlock(par3, par4, par5, Blocks.water);
-            }
-        
+    if (material.blocksMovement() || material.isLiquid()) {
+      par1World.setBlock(par3, par4, par5, Blocks.water);
     }
-	
-	@Override
-	  public void updateTick(World par1World, int par2, int par3, int par4, Random par5Random)
-	    {
-	        if (true)
-	        {
-	            if (par1World.provider.isHellWorld)
-	            {
-	                par1World.setBlockToAir(par2, par3, par4);
-	                return;
-	            }
+  }
 
-	            this.dropBlockAsItem(par1World, par2, par3, par4, par1World.getBlockMetadata(par2, par3, par4), 0);
-	            par1World.setBlock(par2, par3, par4, Blocks.water);
-	        }
-	    }
+  @Override
+  public void updateTick(World par1World, int par2, int par3, int par4,
+                         Random par5Random) {
+    if (true) {
+      if (par1World.provider.isHellWorld) {
+        par1World.setBlockToAir(par2, par3, par4);
+        return;
+      }
 
+      this.dropBlockAsItem(par1World, par2, par3, par4,
+                           par1World.getBlockMetadata(par2, par3, par4), 0);
+      par1World.setBlock(par2, par3, par4, Blocks.water);
+    }
+  }
 }
--- src/main/java/flaxbeard/thaumicexploration/misc/brazier/SoulBrazierQueueData.java
+++ src/main/java/flaxbeard/thaumicexploration/misc/brazier/SoulBrazierQueueData.java
@@ -4,18 +4,17 @@ import java.util.UUID;
 
 public class SoulBrazierQueueData {
 
-	public final UUID aOwner;
-	public final int aQueuedWarpToAdd;
-	public final int aTileX;
-	public final int aTileY;
-	public final int aTileZ;
-	
-	public SoulBrazierQueueData(UUID aPlayer, int aWarp, int aX, int aY, int aZ) {
-		aOwner = aPlayer;
-		aQueuedWarpToAdd = aWarp;
-		aTileX = aX;
-		aTileY = aY;
-		aTileZ = aZ;
-	}
-	
+  public final UUID aOwner;
+  public final int aQueuedWarpToAdd;
+  public final int aTileX;
+  public final int aTileY;
+  public final int aTileZ;
+
+  public SoulBrazierQueueData(UUID aPlayer, int aWarp, int aX, int aY, int aZ) {
+    aOwner = aPlayer;
+    aQueuedWarpToAdd = aWarp;
+    aTileX = aX;
+    aTileY = aY;
+    aTileZ = aZ;
+  }
 }
